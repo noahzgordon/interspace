9bd9738a2d738a5209e9bb21a86c90f3d86ae6a4
Noah Gordon <noahzacharygordon@gmail.com>
2018-08-21 13:06:43 -0400

    First pass at planetary orbits

--- src/Main.elm
+++ src/Main.elm
@@ -20,19 +20,37 @@ main =
 
 
 
-{- MODEL -}
+-- 1 px = 5,000 km
 
 
 systemSize =
-    10000
+    6000000000 / 5000
+
+
+orbits =
+    { mercury = 57910000 / 5000
+    , venus = 108200000 / 5000
+    , earth = 149600000 / 5000
+    , mars = 227900000 / 5000
+    , ceresBelt = 413800000 / 5000
+    , jupiter = 778600000 / 5000
+    , saturn = 1433000000 / 5000
+    , uranus = 2877000000 / 5000
+    , neptune = 4503000000 / 5000
+    , pluto = 5874000000 / 5000
+    }
 
 
 minScale =
-    0.15
+    0.0015
 
 
 maxScale =
     2.25
+
+
+
+{- MODEL -}
 
 
 type alias Model =
@@ -113,8 +131,17 @@ playView model =
                     , onWheel ScrolledMouseWheel
                     ]
                     [ g [ transform ("translate(" ++ ((1 - model.scale) * model.focalPoint.x |> String.fromFloat) ++ "," ++ ((1 - model.scale) * model.focalPoint.y |> String.fromFloat) ++ ") scale(" ++ (String.fromFloat model.scale) ++ ")") ]
-                        [ Svg.rect [ fill "black", width "10000", height "10000" ] []
-                        , Svg.circle [ fill "yellow", r "100", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat model.focalPoint.y) ] []
+                        [ Svg.rect [ fill "black", width (String.fromFloat systemSize), height (String.fromFloat systemSize) ] []
+                        , Svg.circle [ fill "yellow", r "500", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat model.focalPoint.y) ] []
+                        , Svg.circle [ fill "#B1ADAD", r "200", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat <| model.focalPoint.y - orbits.mercury) ] []
+                        , Svg.circle [ fill "#DE5F25", r "200", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat <| model.focalPoint.y - orbits.venus) ] []
+                        , Svg.circle [ fill "#182A61", r "200", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat <| model.focalPoint.y - orbits.earth) ] []
+                        , Svg.circle [ fill "#B53B03", r "200", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat <| model.focalPoint.y - orbits.mars) ] []
+                        , Svg.circle [ fill "#C1844D", r "200", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat <| model.focalPoint.y - orbits.jupiter) ] []
+                        , Svg.circle [ fill "#C1B494", r "200", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat <| model.focalPoint.y - orbits.saturn) ] []
+                        , Svg.circle [ fill "#D3F9FA", r "200", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat <| model.focalPoint.y - orbits.uranus) ] []
+                        , Svg.circle [ fill "#3454DF", r "200", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat <| model.focalPoint.y - orbits.neptune) ] []
+                        , Svg.circle [ fill "#E9E8D2", r "200", cx (String.fromFloat model.focalPoint.x), cy (String.fromFloat <| model.focalPoint.y - orbits.pluto) ] []
                         ]
                     ]
 
